4e1fc3116af1ef13776a936385c137c36325c7c9
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-01-05 13:45:41 +0200

    Update upgrader.sh

--- upgrader.sh
+++ upgrader.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 rm /usr/bin/upgrade
 git clone https://github.com/TheSonicMaster/massos-upgrade.git /tmp/upgrade
-chmod 755 upgrade
 cd /tmp/upgrade/
 mv massos-upgrade upgrade
 cp upgrade /usr/bin
 cd /tmp
 rm -r upgrade
-clear
-upgrade

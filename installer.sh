e3d865935dbfe00938ad1452d951b4f2dd729e46
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-01-11 19:03:06 +0200

    Delete installer.sh

--- installer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/TheSonicMaster/massos-upgrade.git /tmp/upgrade
-cd /tmp/upgrade/
-mv massos-upgrade upgrade
-cp upgrade /usr/bin
-cd /tmp
-rm -r upgrade
-rm -r uiu

d185d1b1877732bd1fe74d512d782e97ef8e54d4
RaptaG <77157639+RaptaG@users.noreply.github.com>
2022-01-21 15:30:23 +0200

    Update notification.sh

--- notification.sh
+++ notification.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+
+# MassOS-uiu, an utility all about MassOS system upgrades
+# Fell free to use my code, but make sure to mention me with the way below:
+# (Used code from RaptaG -> https://github.com/RaptaG)
+
+# Cloning temporaly https://raw.githubusercontent.com/TheSonicMaster/MassOS/main/utils/massos-release to check for new releases
 cd /tmp/
 curl -Os https://raw.githubusercontent.com/TheSonicMaster/MassOS/main/utils/massos-release
+
+# Checking for new releases
 diff /etc/massos-release /tmp/massos-release &>/dev/null
   if [ $? -ne 1 ]; then
   	cd /tmp/
   	rm massos-release
     exit 0
   fi
+
+# Sending the notification
 notify-send 'MassOS Upgrade' 'A new release of MassOS was found!' -u critical -i system-upgrade
+
+# Removing the temporaly file
 cd /tmp/
 rm massos-release
